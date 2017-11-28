0794404c263e1dbe2c3e7582d223a05c1ce7b58a
jsw4795 <33516979+jsw4795@users.noreply.github.com>
2017-11-28 20:29:47 +0900

    asd

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,25 +1,25 @@
 <?php
 // Check for empty fields
-if(empty($_POST['이름'])  		||
-   empty($_POST['이메일']) 		||
-   empty($_POST['휴대폰 번호']) 		||
-   empty($_POST['메세지'])	||
-   !filter_var($_POST['이메일'],FILTER_VALIDATE_EMAIL))
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
 	return false;
    }
 	
-$name = strip_tags(htmlspecialchars($_POST['이름']));
-$email_address = strip_tags(htmlspecialchars($_POST['이메일 주소']));
-$phone = strip_tags(htmlspecialchars($_POST['휴대폰 번호']));
-$message = strip_tags(htmlspecialchars($_POST['메세지']));
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'jsw4795@naver.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: jsw4795@naver.com"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
